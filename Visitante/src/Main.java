99e243e0ec76e3de8f2f6bc2d2ebbb59099b8f42
Andresrr24 <andres.rueda@uip.edu.pa>
2024-07-08 21:31:45 -0500

    Add files via upload

---
+++ Visitante/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String nombre = "Andres Rueda";
+        int cedula = 1014861220;
+        byte edad = 18;
+        boolean esResidente = false;
+        double torrePiso = 2.6;
+        float altura = 1.70F;
+        long id = 13564210481L;
+
+       System.out.println("Datos del visitante:");
+       System.out.println("Nombre: " + nombre);
+       System.out.println("Cedula: " + cedula);
+       System.out.println("Edad: " + edad);
+       System.out.println("Residente: " + esResidente);
+       System.out.println("Torre,piso: " + torrePiso);
+       System.out.println("Altura: " + altura);
+       System.out.println("id: " + id);
+    }
+}
